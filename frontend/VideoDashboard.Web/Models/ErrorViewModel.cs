49b72260e577bbda3a141c7936b53693e75fc02d
Marc Gabriel N. Badilla <62585845+MarcBad98@users.noreply.github.com>
2025-11-17 18:54:40 -0500

    Created sample .NET Core MVC project

---
+++ frontend/VideoDashboard.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VideoDashboard.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
